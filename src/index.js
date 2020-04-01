3741202a4ae352c4ac54ebd4b325ee2a59557ff7
gcampbell391 <gcampbell391@gmail.com>
2020-04-01 13:51:36 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+
 });
+
+document.addEventListener("DOMContentLoaded", () => {});
+//get form
+const form1 = document.querySelector('#create-task-form');
+//access event.target.value
+form1.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //get input
+    const taskEntered = event.target.querySelector('#new-task-description').value;
+    //create li with textContent
+    const li1 = document.createElement('li');
+    li1.innerText = taskEntered
+        //assign parent to variable
+    const parent = document.querySelector('#tasks');
+    //append li to parent
+    parent.appendChild(li1)
+        //clear form input
+    form1.reset()
+})
